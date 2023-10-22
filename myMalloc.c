87beb64733f7290b9c2f6b4a27b94365266a9c20
Giovanni Karra <karragiovanni@gmail.com>
2023-10-22 14:06:08 +0200

    malloc fait, faut juste le debug

--- myMalloc.c
+++ myMalloc.c
@@ -1,4 +1,5 @@
-#define MEMORY_SIZE 64000
+#define MEMORY_SIZE (uint16_t)500
+#define NO_SPACE_FOUND -1
 
 #include <stdio.h>
 #include <stdint.h>
@@ -7,10 +8,13 @@
 uint8_t MY_HEAP[MEMORY_SIZE];
 
 void init() {
-    //remise à 0 pour éviter d'avoir des fausses méta-données
+    // remise à 0 pour éviter d'avoir des fausses méta-données
     for (int i = 0; i < MEMORY_SIZE; i++) {
         MY_HEAP[i] = 0;
     }
+    // premier bloc = libre et fait la taille de toute le mémoire
+    MY_HEAP[0] = (uint8_t)(MEMORY_SIZE >> 8);
+    MY_HEAP[1] = (uint8_t)(MEMORY_SIZE & 254);
 }
 
 // revoie 1 si le bloc est libre, 0 sinon
@@ -23,18 +27,54 @@ uint16_t get_size(uint16_t address) {
     return MY_HEAP[address-2] << 8 + MY_HEAP[address-1] & 254;
 }
 
-uint16_t search_for_block(size_t size, uint16_t address) {
-    //if (!is_free(address)) search_for_block(size, )
+uint16_t search_for_free_block(size_t size, uint16_t start_address) {
+    if (start_address > MEMORY_SIZE) return NO_SPACE_FOUND;
+
+    if (!is_free(start_address))
+        return search_for_free_block(size, start_address + get_size(start_address));
+
+    uint16_t current_size = get_size(start_address);
+
+    uint16_t next_address = start_address;
+    while (current_size-4 < size) {
+        next_address += current_size;
+        if (!is_free(next_address)) return search_for_free_block(size, next_address);
+
+        current_size += get_size(next_address) - 4;
+    }
+
+    return start_address;
 }
 
 void *my_malloc(size_t size) {
-    uint16_t address = search_for_block(size, 2);
+    // si la taille est impaire, ajouter 1 pour la rendre paire
+    size += size % 2;
+    uint16_t address = search_for_free_block(size, 2);
+
+    // méta-données de début
+    MY_HEAP[address-2] = (uint8_t)(size >> 8);
+    MY_HEAP[address-1] = (uint8_t)(size & 254)+1;
+    // méta-données de fin
+    MY_HEAP[address+size] = (uint8_t)(size >> 8);
+    MY_HEAP[address+size+1] = (uint8_t)(size & 254)+1;
 }
 
 void free(void *pointer) {
 
 }
 
+void print_memory() {
+    printf("|");
+    for (int i = 0; i < MEMORY_SIZE; i++)
+        printf(" %d |", MY_HEAP[i]);
+    printf("\n");
+}
+
 int main(int argc, char **argv) {
+    init();
+    my_malloc(10);
+    print_memory();
+    my_malloc(300);
+    print_memory();
     return 0;
 }
