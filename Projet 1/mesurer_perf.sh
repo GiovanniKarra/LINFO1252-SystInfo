ea90fbdded5547512810313d955aa0057da14aa5
Giovanni Karra <karragiovanni@gmail.com>
2023-11-19 15:52:20 +0100

    amélioration du script de test

--- Projet 1/mesurer_perf.sh
+++ Projet 1/mesurer_perf.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 FICHIER_MESURES_PHILO="mesures_philo.csv"
+NB_MESURES=5
 
-echo "NombreDeThreads,Essai 1,Essai 2,Essai 3,Essai 4,Essai 5" > $FICHIER_MESURES_PHILO
+TITRE="NombreDeThreads"
+for i in $(seq 1 $NB_MESURES); do
+    TITRE="$TITRE,Essai$i"
+done
+echo $TITRE > $FICHIER_MESURES_PHILO
+
+#echo "NombreDeThreads,Essai 1,Essai 2,Essai 3,Essai 4,Essai 5" > $FICHIER_MESURES_PHILO
 make clean
 make philo
 
