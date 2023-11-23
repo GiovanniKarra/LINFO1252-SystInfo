06707a2b6f3d5cffdc58bd4f060fe45458443496
Giovanni Karra <karragiovanni@gmail.com>
2023-11-24 00:18:30 +0100

    bo plots

--- Projet 1/plots.py
+++ Projet 1/plots.py
@@ -4,13 +4,16 @@ import pandas as pd
 nb_de_threads =\
     ["2 Threads", "4 Threads", "8 Threads", "16 Threads", "32 Threads", "64 Threads"]
 
+plt.figure(figsize=(7, 6))
+plt.subplots_adjust(hspace=0.5)
+
 
 """PLOT PHILO"""
 data_philo : pd.DataFrame = pd.read_csv("mesures_philo.csv")
 essais_philo = [data_philo[f"Essai{i}"] for i in range(1, 6)]
 #moyenne_par_thread = essais.transpose().mean()
 
-plt.figure()
+plt.subplot(311)
 
 plt.title("Temps d'execution du problème des philosophes")
 
@@ -19,19 +22,17 @@ plt.grid(True, color="grey", linestyle=":", linewidth=1)
 #plt.xlabel("Nombre de threads")
 plt.ylabel("temps d'execution [s]")
 
-plt.xticks(range(1, 7), nb_de_threads)
+plt.xticks(range(1, len(nb_de_threads)+1), nb_de_threads)
 
 for i in range(5):
-    plt.scatter(range(1, 7), essais_philo[i])
-
-plt.show()
+    plt.scatter(range(1,  len(nb_de_threads)+1), essais_philo[i])
 
 
 """PLOT PRODCONS"""
 data_prodcons : pd.DataFrame = pd.read_csv("mesures_prodcons.csv")
 essais_prodcons = [data_prodcons[f"Essai{i}"] for i in range(1, 6)]
 
-plt.figure()
+plt.subplot(312)
 
 plt.title("Temps d'execution du problème du producteur consommateur")
 
@@ -40,20 +41,17 @@ plt.grid(True, color="grey", linestyle=":", linewidth=1)
 #plt.xlabel("Nombre de threads")
 plt.ylabel("temps d'execution [s]")
 
-plt.xticks(range(1, 6), 
-           ["2 Threads", "4 Threads", "8 Threads", "16 Threads", "32 Threads"])
+plt.xticks(range(1, len(nb_de_threads)), nb_de_threads[:-1])
 
 for i in range(5):
-    plt.scatter(range(1, 6), essais_prodcons[i])
-
-plt.show()
+    plt.scatter(range(1, len(nb_de_threads)), essais_prodcons[i])
 
 
 """PLOT READWRITE"""
 data_readwrite : pd.DataFrame = pd.read_csv("mesures_readwrite.csv")
 essais_readwrite = [data_readwrite[f"Essai{i}"] for i in range(1, 6)]
 
-plt.figure()
+plt.subplot(313)
 
 plt.title("Temps d'execution du problème du lecteur écrivain")
 
@@ -62,10 +60,9 @@ plt.grid(True, color="grey", linestyle=":", linewidth=1)
 #plt.xlabel("Nombre de threads")
 plt.ylabel("temps d'execution [s]")
 
-plt.xticks(range(1, 7), 
-           ["2 Threads", "4 Threads", "8 Threads", "16 Threads", "32 Threads", "64 Threads"])
+plt.xticks(range(1,  len(nb_de_threads)+1), nb_de_threads)
 
 for i in range(5):
-    plt.scatter(range(1, 7), essais_readwrite[i])
+    plt.scatter(range(1,  len(nb_de_threads)+1), essais_readwrite[i])
 
 plt.show()
