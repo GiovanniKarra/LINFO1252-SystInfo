e6e2f784279e3a6be0c3496e389d828c4a5567fd
Giovanni Karra <karragiovanni@gmail.com>
2023-12-05 14:22:43 +0100

    modularisation du script bash

--- Projet 1/mesurer_perf.sh
+++ Projet 1/mesurer_perf.sh
@@ -6,91 +6,43 @@ FICHIER_MESURES_PRODCONS="mesures_prodcons.csv"
 FICHIER_MESURES_READWRITE="mesures_readwrite.csv"
 FICHIER_MESURES_SPINLOCK="mesures_spinlock.csv"
 
-# # PHILO
-# TITRE="NombreDeThreads"
-# for i in $(seq 1 $NB_MESURES); do
-#     TITRE="$TITRE,Essai$i"
-# done
-# echo $TITRE > $FICHIER_MESURES_PHILO
+mesure_perf() {
+    TITRE="NombreDeThreads"
+    for i in $(seq 1 $NB_MESURES); do
+        TITRE="$TITRE,Essai$i"
+    done
+    echo $TITRE > $1
 
-# make clean
-# make philo
+    make clean
+    make $2
 
-# for i in 2 4 8 16 32 64
-# do
-#     LIGNE=$i
-#     for j in 1 2 3 4 5
-#     do
-#         INFO=`/usr/bin/time -f %e ./philosophes $i 2>&1`
-#         LIGNE="$LIGNE,$INFO"
-#     done
-#     echo $LIGNE >> $FICHIER_MESURES_PHILO
-# done
-
-
-# # CONSPROD
-# TITRE="NombreDeThreads"
-# for i in $(seq 1 $NB_MESURES); do
-#     TITRE="$TITRE,Essai$i"
-# done
-# echo $TITRE > $FICHIER_MESURES_PRODCONS
-
-# make clean
-# make prodcons
-
-# for i in 2 4 8 16 32 64
-# do
-#     LIGNE=$i
-#     for j in 1 2 3 4 5
-#     do
-#         a=`expr $i / 2`
-#         INFO=`/usr/bin/time -f %e ./prodcons $a $a 2>&1`
-#         LIGNE="$LIGNE,$INFO"
-#     done
-#     echo $LIGNE >> $FICHIER_MESURES_PRODCONS
-# done
+    for i in 2 4 8 16 32 64
+    do
+        LIGNE=$i
+        for j in 1 2 3 4 5
+        do
+            if [ $3 = 1 ]; then
+                INFO=`/usr/bin/time -f %e ./$2 $i 2>&1`
+                LIGNE="$LIGNE,$INFO"
+            else
+                a=`expr $i / 2`
+                INFO=`/usr/bin/time -f %e ./$2 $a $a 2>&1`
+                LIGNE="$LIGNE,$INFO"
+            fi
+        done
+        echo $LIGNE >> $1
+    done
+}
 
 
-# # READWRITE
-# TITRE="NombreDeThreads"
-# for i in $(seq 1 $NB_MESURES); do
-#     TITRE="$TITRE,Essai$i"
-# done
-# echo $TITRE > $FICHIER_MESURES_READWRITE
+# PHILO
+# mesure_perf $FICHIER_MESURES_PHILO philo 1
 
-# make clean
-# make reader_writer
-
-# for i in 2 4 8 16 32 64
-# do
-#     LIGNE=$i
-#     for j in 1 2 3 4 5
-#     do
-#         a=`expr $i / 2`
-#         INFO=`/usr/bin/time -f %e ./reader_writer $a $a 2>&1`
-#         LIGNE="$LIGNE,$INFO"
-#     done
-#     echo $LIGNE >> $FICHIER_MESURES_READWRITE
-# done
-
+# CONSPROD
+# mesure_perf $FICHIER_MESURES_PRODCONS prodcons 2
 
 # READWRITE
-TITRE="NombreDeThreads"
-for i in $(seq 1 $NB_MESURES); do
-    TITRE="$TITRE,Essai$i"
-done
-echo $TITRE > $FICHIER_MESURES_SPINLOCK
+# mesure_perf $FICHIER_MESURES_READWRITE reader_writer 1
 
-make clean
-make test_and_set
-
-for i in 2 4 8 16 32 64
-do
-    LIGNE=$i
-    for j in 1 2 3 4 5
-    do
-        INFO=`/usr/bin/time -f %e ./test_and_set $i 2>&1`
-        LIGNE="$LIGNE,$INFO"
-    done
-    echo $LIGNE >> $FICHIER_MESURES_SPINLOCK
-done
+# TEST AND SET
+mesure_perf $FICHIER_MESURES_SPINLOCK test_and_set 1
